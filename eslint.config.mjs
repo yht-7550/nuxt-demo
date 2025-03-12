ec8b2e326168ec3beba9f6887ebbe7a10a9b12cb
yht <1589016903@qq.com>
2025-03-12 17:26:03 +0800

    chore: 依赖包升级

---
+++ eslint.config.mjs
@@ -0,0 +1,31 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  type: 'lib',
+  stylistic: {
+    indent: 2,
+    quotes: 'single',
+    overrides: {
+      'antfu/top-level-function': 'off',
+      'no-console': 'warn',
+      'unused-imports/no-unused-vars': 'warn',
+      'ts/no-namespace': 'off',
+      'ts/no-use-before-define': 'off',
+      'ts/method-signature-style': 'off',
+    },
+  },
+  typescript: true,
+  vue: {
+    overrides: {
+      'vue/v-slot-style': 'off',
+    },
+  },
+  formatters: {
+    css: true,
+    prettierOptions: {
+      tabWidth: 2,
+      useTabs: false,
+    },
+  },
+  ignores: ['**/node_modules/**', '**/dist/**', 'README.md'],
+})
---
+++ pages/index.vue
@@ -0,0 +1,31 @@
+<script setup lang="ts">
+const count = ref(0)
+
+const handleCount = () => {
+  count.value++
+}
+</script>
+
+<template>
+  <div class="p-4 flex">
+    <div>
+      <div class="w-80">
+        <UCard>
+          <div class="h-40">
+            <Icon class="text-blue-500" name="material-symbols:add-location-rounded" size="2rem" />
+            数量: {{ count }}
+          </div>
+        </UCard>
+      </div>
+      <UButton class="mt-4" @click="handleCount">
+        修改count
+      </UButton>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+  color: red;
+}
+</style>
